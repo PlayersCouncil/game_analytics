d2a5febfffcf126cc9ec04da4fc0b32d4791d41c
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2026-01-02 20:02:35 -0600

    Fixed precompute log going to the wrong place

--- precompute.py
+++ precompute.py
@@ -28,7 +28,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('precompute.log'),
+        logging.FileHandler('./logs/precompute.log'),
         logging.StreamHandler()
     ]
 )
